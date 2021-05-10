168a74791e3bb695611093fb6dac0c84c8927dcc
Richard Schneeman <rschneeman@salesforce.com>
2021-05-10 12:52:05 -0500

    Clean up Java test app
    - Remove unused testing file
    - Reduce dependencies

--- repos/jvm/sf-fx-template-java/src/main/java/com/example/ExampleFunction.java
+++ repos/jvm/sf-fx-template-java/src/main/java/com/example/ExampleFunction.java
@@ -4,8 +4,6 @@ import com.salesforce.functions.jvm.sdk.Context;
 import com.salesforce.functions.jvm.sdk.InvocationEvent;
 import com.salesforce.functions.jvm.sdk.SalesforceFunction;
 import com.salesforce.functions.jvm.sdk.data.Record;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,7 +12,6 @@ import java.util.List;
  * Describe ExampleFunction here.
  */
 public class ExampleFunction implements SalesforceFunction<FunctionInput, FunctionOutput> {
-  private static final Logger LOGGER = LoggerFactory.getLogger(ExampleFunction.class);
 
   @Override
   public FunctionOutput apply(InvocationEvent<FunctionInput> event, Context context)
